664be6a6ea7614996b007f8d6abd6cc8dcb68252
Rohit Kumar <rohit.ad.kumar@oracle.com>
2021-09-19 01:50:31 +0530

    Add youtube_search.py Refactor hit_rewind.json

--- src/main/playlist_to_youtube.py
+++ src/main/playlist_to_youtube.py
@@ -19,7 +19,7 @@ class PlaylistToYoutube(Resource):
 
 
 def main():
-    path = '/Users/a0b0hst/PycharmProjects/playlist-to-youtube-local/src/resources/hit_rewind.json'
+    path = 'hit_rewind.json'
     f = open(path, )
     spotify_playlist = json.load(f)
     playlist_name, list_of_songs = parse_spotify_playlist(spotify_playlist)
