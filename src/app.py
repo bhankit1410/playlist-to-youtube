54ebc01ecc7503d0d1574fc7751e1dd1acf410f8
Rohit Kumar <rohit.ad.kumar@oracle.com>
2021-09-19 00:48:28 +0530

    Refactor directory structure

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from src.resources.main.playlist_to_youtube import PlaylistToYoutube
+from src.main.playlist_to_youtube import PlaylistToYoutube
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
